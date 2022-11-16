e8f94b65595771b6e4e710b0b8873ae59dc2b19c
Cydeo-Emir <emirizg77@gmail.com>
2022-11-16 09:28:29 +0100

    ContactsStepDefinitions

---
+++ src/test/java/com/vytrack/step_definition/ContactsStepDefinitions.java
@@ -0,0 +1,23 @@
+package com.vytrack.step_definition;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ContactsStepDefinitions {
+
+    @Given("the user logged in as {string}")
+    public void the_user_logged_in_as(String string) {
+
+    }
+
+    @When("the user navigates to {string} {string}")
+    public void the_user_navigates_to(String string, String string2) {
+
+    }
+    @Then("the title contains {string}")
+    public void the_title_contains(String string) {
+
+    }
+
+}
